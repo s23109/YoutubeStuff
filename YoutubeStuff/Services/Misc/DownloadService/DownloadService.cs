a6b0c6cff2ae2fa3a77c288da3581b815a1b3fac
D K <s23109@pjwstk.edu.pl>
2023-09-26 23:11:31 +0200

    DownloadService regex add

---
+++ YoutubeStuff/Services/Misc/DownloadService/DownloadService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using YoutubeStuff.Utils;
+
+namespace YoutubeStuff.Services.Misc.DownloadService
+{
+    public class DownloadService : IDownloadService
+    {
+        //11 vid id, 34 list id, pomijając query string
+        private Regex regexSingleVideo = new Regex("^https:\\/\\/www\\.youtube\\.com\\/watch\\?v\\=.{11}", RegexOptions.Singleline);
+        private Regex regexPlaylistVideo = new Regex("^https:\\/\\/www\\.youtube\\.com\\/watch\\?v\\=.{11}&list=.{34}$", RegexOptions.Singleline);
+
+        //either 
+        private Regex regexCorrectUrl = new Regex("^https:\\/\\/www\\.youtube\\.com\\/watch\\?v\\=.{11}(&list=.{34})?$", RegexOptions.Singleline);
+
+
+        public bool IsCorrectUrl(string url)
+        {
+            return regexCorrectUrl.IsMatch(url);
+        }
+    }
+}
---
+++ YoutubeStuff/Services/Misc/DownloadService/IDownloadService.cs
@@ -0,0 +1,10 @@
+﻿using YoutubeStuff.Utils;
+
+namespace YoutubeStuff.Services.Misc.DownloadService
+{
+    public interface IDownloadService
+    {
+        bool IsCorrectUrl(string url);
+
+    }
+}
