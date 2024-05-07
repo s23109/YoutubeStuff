b135fd77f9c594e61b093c5cf59634e60dc628e3
DK <s23109@pjwstk.edu.pl>
2024-05-07 21:38:23 +0200

    NavElement viewmodel

---
+++ YoutubeStuff/YoutubeStuff/ViewModels/NavElement.cs
@@ -0,0 +1,12 @@
+﻿namespace YoutubeStuff.ViewModels
+{
+    public class NavElement
+    {
+        //add support for icons?
+
+        public string Title { get; set; }
+        public string Destination { get; set; }
+
+
+    }
+}
